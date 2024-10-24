bafdf69a1038dbf9b6c158925f5ef13b9a04c634
kchan5071 <kchan5071@sdsu.edu>
2024-10-24 16:43:14 -0700

    added some arg parsing

--- Makefile
+++ Makefile
@@ -16,20 +16,11 @@ pagetrace:
 # link output files to pagetrace executable
 	gcc main.o tracereader.o log.o page_table.o page_table_params.o -o pagetrace
 
-run-complete:
-	./pagetrace trace_files/trace.tr "7 15" > results/page_trage_7_15.txt
-	./pagetrace trace_files/trace.tr "2 3 4 5 8" > results/page_trage_2_3_4_5_8.txt
-	./pagetrace trace_files/trace.tr "6 2 4 2 2" > results/page_trage_6_2_4_2_2.txt
-	./pagetrace trace_files/trace.tr "3 5 7" > results/page_trage_3_5_7.txt
-	./pagetrace trace_files/trace.tr "2 2 2 2 2 2 2 2 2 2 2" > results/page_trage_2_2_2_2_2_2_2_2_2_2_2.txt
-	./pagetrace trace_files/trace.tr "1" > results/page_trage_1.txt
-	./pagetrace trace_files/trace.tr "20" > results/page_trage_20.txt
-	./pagetrace trace_files/trace.tr "12 12" > results/page_trage_12_12.txt
-	./pagetrace trace_files/trace.tr "4 4 4 4" > results/page_trage_4_4_4_4.txt
-	./pagetrace trace_files/trace.tr "8 8 8" > results/page_trage_8_8_8.txt
-
 run:
-	./pagetrace trace_files/trace.tr "7 15"
+	make pagetrace
+	./pagetrace -o TEST -n 500 -c 20 trace_files/trace.tr "7 15" 
+	make clean
+
 
 compile_run:
 	make clean
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "tracereader.h"
 #include "log.h"
 #include "page_table.h"
@@ -10,11 +11,66 @@
  * 827673009
  */
 
+struct args {
+    //cache capcaity, 0 by default
+    int cache_capacity;
+    //does all addresses by default, which  is encoded as -1
+    int number_of_addresses;
+    //default is "none"
+    char* output_mode;
+
+};
+
+static struct args* parse_opt(int argc, char** argv) {
+    struct args* args = malloc(sizeof(struct args));
+    args->cache_capacity = 0;
+    args->number_of_addresses = -1;
+    args->output_mode = "none";
+    int opt;
+    while ((opt = getopt(argc, argv, "c:n:o:")) != -1) {
+        switch (opt) {
+            case 'c':
+                args->cache_capacity = atoi(optarg);
+                break;
+            case 'n':
+                args->number_of_addresses = atoi(optarg);
+                break;
+            case 'o':
+                args->output_mode = optarg;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-c cache_capacity] [-n number_of_addresses] [-o output_mode] filename\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    return args;
+}
+
+static char* get_filename(int argc, char** argv) {
+    //search args for .tr suffix
+    for (int i = 1; i < argc; i++) {
+        if (strstr(argv[i], ".tr") != NULL) {
+            return argv[i];
+        }
+    }
+    return NULL;
+}
+
 int main(int argc, char** argv) {
 
     //read args
-    char *filename = argv[1];
     int depth = 0;
+
+    //parse arguments
+    struct args* args = parse_opt(argc, argv);
+    printf("cache_capacity: %d\n", args->cache_capacity);
+    printf("number_of_addresses: %d\n", args->number_of_addresses);
+    printf("output_mode: %s\n", args->output_mode);
+
+    //check for filename
+    char* filename = get_filename(argc, argv);
+    printf("filename: %s\n", filename);
+
 
     //create page table
     page_table* table = build_page_table(argv, &depth);
